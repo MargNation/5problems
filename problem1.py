897f71b1eb8ee69300c5f2658c57e699b578131a
Margaret Harriman <margaret.w.harriman@gmail.com>
2016-12-31 03:59:07 -0500

    Answers to Problem 1

---
+++ problem1.py
@@ -0,0 +1,27 @@
+#Problem 1
+#
+#Write three functions that compute the sum of the 
+#numbers in a given list using a for-loop, 
+#a while-loop, and recursion.
+
+def forLoop(arr):
+    runningSum = 0
+    for i in range(len(arr)):
+        runningSum += arr[i]
+    return runningSum
+
+def whileLoop(arr):
+    runningSum = 0
+    counter = 0
+    while (counter != len(arr)):
+        runningSum += arr[counter]
+        counter += 1
+    return runningSum
+
+def recursive(arr):
+    return
+
+testArray = [1, 2, 3, 4, 5] #sum is 15
+print forLoop(testArray)
+print whileLoop(testArray)
+print recursive(testArray)
