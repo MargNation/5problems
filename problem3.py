bf3592ae29179d54cf84d1b49cd487568a25e299
Margaret Harriman <margaret.w.harriman@gmail.com>
2016-12-31 17:32:08 -0500

    Update problem3.py

---
+++ problem3.py
@@ -0,0 +1,20 @@
+#Problem 3
+#
+#Write a function that computes the list of the 
+#first 100 Fibonacci numbers. By definition, the first 
+#two numbers in the Fibonacci sequence are 0 and 1, and 
+#each subsequent number is the sum of the previous two. 
+#
+#As an example, here are the first 10 Fibonnaci numbers: 
+#0, 1, 1, 2, 3, 5, 8, 13, 21, and 34.
+
+def fibo(n):
+    
+    array = [0, 1]
+    
+    while (len(array) < n):
+        array.append(array[len(array) - 2] + array[len(array) - 1])
+        
+    return array
+    
+print fibo(100)
