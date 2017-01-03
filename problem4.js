aff0c1ef56da7c18dc63a6a445a9591cf726f657
Margaret Harriman <margaret.w.harriman@gmail.com>
2017-01-02 19:16:38 -0500

    Update problem4.js

---
+++ problem4.js
@@ -0,0 +1,27 @@
+// Problem 4
+
+// Write a function that given a list of non negative integers, 
+// arranges them such that they form the largest possible number. 
+
+// For example, given [50, 2, 1, 9], the largest formed number is 95021.
+
+function findLargestNum(arr) {
+  var firstNum = arr[0].toString();
+  for (var i = 1; i < arr.length; i++) {
+    if (firstNum + arr[i] > arr[i] + firstNum) {
+      firstNum += arr[i];
+      } else {
+        firstNum = (arr[i] + firstNum);
+      }
+  }
+  return firstNum;
+}
+
+var testArray1 = [50, 2, 1, 9];
+console.log('TestArray1: ', findLargestNum(testArray1));
+var testArray2 = [9, 90];
+console.log('TestArray2: ', findLargestNum(testArray2));
+var testArray3 = [8, 8, 8];
+console.log('TestArray2: ', findLargestNum(testArray3));
+var testArray4 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log('TestArray2: ', findLargestNum(testArray4));
